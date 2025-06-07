fcfef190a33893478ae6f9ccf5344faf008440dd
Kevin Medeiros <108627831+medeirosnvk@users.noreply.github.com>
2025-06-06 21:21:08 -0300

    novo teste dois

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  console.log("Hello World");
-  res.send("Hello World");
+  console.log("Hello World teste dois");
+  res.send("Hello World teste dois");
 });
 
 app.listen(3000, () => {
